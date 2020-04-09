2c7f0ccd5140bca0cbe3961aecbaeded4ef753ae
knowbots <59176261+knowbots@users.noreply.github.com>
2020-04-09 06:38:24 -0400

    javascript for form

---
+++ scripts.js
@@ -0,0 +1,24 @@
+
+// user information enters into form goes somewhere useful for me
+
+// when user submit the form clears and they get a notification of success
+
+let formEntry = []; // intialize array to store form data
+
+// create variables to select the form button and form entries
+const contactForm = document.getElementById('contact-form');
+const submitButton = document.getElementById('contact-submit');
+const userMessage = document.getElementById('msg');
+const userName = document.getElementById('name');
+const userEmail = document.getElementById('mail');
+
+function submitForm() {
+  alert("Success!");
+  contactForm.reset();
+}
+
+submitButton.addEventListener('click', submitForm);
+
+
+
+// when user submits successfully, googletagmanager fires an event and a goal
