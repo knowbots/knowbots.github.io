82e99f27183467a13aef9e7c61c09933053843d1
knowbots <59176261+knowbots@users.noreply.github.com>
2020-04-26 14:22:16 -0400

    number check project started

---
+++ projects/app.js
@@ -0,0 +1,13 @@
+// must have a way to enter values
+
+// must have a way to randomly generate a number between 0-9
+
+// must have a way to compared two numeric values
+
+// must have a way to check if those values are strictly equal
+
+// must have a way to reset if the values are not equal
+
+// must have a way to disply success
+
+// must have a way to reset the state
